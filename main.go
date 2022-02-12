e1cbd58d9df055c97b7d75cb2f57d118bd07c3f5
Connor Henderson <connorhenderson20@gmail.com>
2022-02-12 13:46:55 -0500

    Start

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var fileName string
+
+	fmt.Println("What is the file name?")
+	fmt.Scan(&fileName)
+	//fmt.Printf("%s", fileName)
+}
+
+func countWords() {
+
+}
+
+func reportResults() {
+
+}
